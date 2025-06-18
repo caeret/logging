d5e8d47d7863d4857bf1261c101aebf6ca42e2a5
Ren Hao <renhao2k@gmail.com>
2025-06-18 13:39:22 +0800

    feat: Update

--- slog/logger.go
+++ slog/logger.go
@@ -7,40 +7,40 @@ import (
 	"github.com/caeret/logging"
 )
 
-var _ logging.Logger = (*logger)(nil)
+var _ logging.Logger = (*Logger)(nil)
 
 func New(l *slog.Logger) logging.Logger {
-	return &logger{l: l}
+	return &Logger{l: l}
 }
 
-type logger struct {
+type Logger struct {
 	l *slog.Logger
 }
 
-func (l *logger) Debug(msg string, args ...any) {
+func (l *Logger) Debug(msg string, args ...any) {
 	l.l.Debug(msg, args...)
 }
 
-func (l *logger) Info(msg string, args ...any) {
+func (l *Logger) Info(msg string, args ...any) {
 	l.l.Info(msg, args...)
 }
 
-func (l *logger) Warn(msg string, args ...any) {
+func (l *Logger) Warn(msg string, args ...any) {
 	l.l.Warn(msg, args...)
 }
 
-func (l *logger) Error(msg string, args ...any) {
+func (l *Logger) Error(msg string, args ...any) {
 	l.l.Error(msg, args...)
 }
 
-func (l *logger) With(fields ...interface{}) logging.Logger {
-	return &logger{l: l.l.With(fields...)}
+func (l *Logger) With(fields ...interface{}) logging.Logger {
+	return &Logger{l: l.l.With(fields...)}
 }
 
-func (l *logger) WithCtx(ctx context.Context) logging.Logger {
+func (l *Logger) WithCtx(ctx context.Context) logging.Logger {
 	return logging.WithCtx(ctx, l)
 }
 
-func (l *logger) Sync() error {
+func (l *Logger) Sync() error {
 	return nil
 }
